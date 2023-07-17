7947257977a09be72d8a020b850745d8eafcbfed
Dqnger <100387311+Dqnger@users.noreply.github.com>
2023-07-17 21:50:22 +0100

    BlockCMD fixed

--- src/main/java/org/screenwork/minestomtest/commands/ToBlockCMD.java
+++ src/main/java/org/screenwork/minestomtest/commands/ToBlockCMD.java
@@ -1,10 +1,11 @@
 package org.screenwork.minestomtest.commands;
 
-import net.minestom.server.MinecraftServer;
+
 import net.minestom.server.command.builder.Command;
+import net.minestom.server.coordinate.Point;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.instance.Instance;
-import net.minestom.server.instance.block.Block;
+
 
 public class ToBlockCMD extends Command {
 
@@ -15,19 +16,17 @@ public class ToBlockCMD extends Command {
             sender.sendMessage("Usage: /to");
         });
 
-
         addSyntax((sender, context) -> {
-            //get block at loc
             final Instance instance = sender.asPlayer().getInstance();
-            final Pos coords = Pos.fromPoint(sender.asPlayer().getLineOfSight(256).stream().filter(block -> instance.getBlock(block).equals(Block.AIR)).findFirst().orElse(null));
-            System.out.println(coords);
 
-            if (coords != null) {
-                sender.asPlayer().teleport(coords);
-            } else {
+            try {
+                Point point = sender.asPlayer().getTargetBlockPosition(256);
+                Pos cords = new Pos(point, sender.asPlayer().getPosition().yaw(), sender.asPlayer().getPosition().pitch());
+
+                sender.asPlayer().teleport(cords);
+            } catch (Exception e) {
                 sender.sendMessage("No block found");
             }
-
         });
     }
 }
