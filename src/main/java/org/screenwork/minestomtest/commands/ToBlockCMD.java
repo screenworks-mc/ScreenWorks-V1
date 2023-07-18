e5ea98876fce19f01c084eed5e936179e51ae03e
Dqnger <100387311+SDqnger@users.noreply.github.com>
2023-07-18 20:50:38 +0100

    Incremented Y level by 1 to prevent spawning inside block

--- src/main/java/org/screenwork/minestomtest/commands/ToBlockCMD.java
+++ src/main/java/org/screenwork/minestomtest/commands/ToBlockCMD.java
@@ -21,6 +21,7 @@ public class ToBlockCMD extends Command {
 
             try {
                 Point point = sender.asPlayer().getTargetBlockPosition(256);
+                point = point.add(0, 1, 0); // Add 1 to the Y axis (so you don't spawn in the block
                 Pos cords = new Pos(point, sender.asPlayer().getPosition().yaw(), sender.asPlayer().getPosition().pitch());
 
                 sender.asPlayer().teleport(cords);
