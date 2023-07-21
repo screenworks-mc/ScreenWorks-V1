e0749d5967776d79eddb36e1e6609e22d3a94cb9
cake5diamond <104602895+Calc-oholic@users.noreply.github.com>
2023-07-21 15:16:18 -0500

    Looping through to add textures

--- src/main/java/org/screenwork/minestomtest/pack/pack.java
+++ src/main/java/org/screenwork/minestomtest/pack/pack.java
@@ -1,5 +1,6 @@
 package org.screenwork.minestomtest.pack;
 
+import net.kyori.adventure.key.Key;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.event.player.PlayerChatEvent;
@@ -8,11 +9,14 @@ import team.unnamed.creative.ResourcePack;
 import team.unnamed.creative.base.Writable;
 import team.unnamed.creative.serialize.minecraft.MinecraftResourcePackWriter;
 import team.unnamed.creative.server.ResourcePackServer;
+import team.unnamed.creative.texture.Texture;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 public class pack {
 
@@ -20,10 +24,14 @@ public class pack {
 
     public pack() {
         try {
+            Textures();
             ResourcePack resourcePack = ResourcePack.create();
-            resourcePack.packMeta(9, "Description!");
+            resourcePack.packMeta(9, "Official pack for ScreenWorks!");
             resourcePack.icon(Writable.file(new File("src/main/java/org/screenwork/minestomtest/pack/assets/my-icon.png")));
             resourcePack.unknownFile("credits.txt", Writable.stringUtf8("ScreenWorks"));
+            for (Texture texture : textures) {
+                resourcePack.texture(texture);
+            }
 
             File resourcePackFile = new File("src/main/java/org/screenwork/minestomtest/pack/resource-pack.zip");
             MinecraftResourcePackWriter.minecraft().writeToZipFile(resourcePackFile, resourcePack);
@@ -56,13 +64,19 @@ public class pack {
                     event.setCancelled(true);
                 }
             });
-
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void Textures() {
+    private List<Texture> textures = new ArrayList<>();
 
+    public void Textures() {
+        Texture texture = Texture.builder()
+                .key(Key.key("namespace", "my_texture.png"))
+                .data(Writable.file(new File("exampleTexture.png")))
+                .build();
+
+        textures.add(texture);
     }
 }
