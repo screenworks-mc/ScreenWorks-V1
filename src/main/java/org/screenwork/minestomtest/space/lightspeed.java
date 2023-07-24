104c34c8c4789bddf746f8bfe98ec86c06b103b3
cake5diamond <104602895+Calc-oholic@users.noreply.github.com>
2023-07-24 10:56:52 -0500

    Attempted fix for world copying

--- src/main/java/org/screenwork/minestomtest/space/lightspeed.java
+++ src/main/java/org/screenwork/minestomtest/space/lightspeed.java
@@ -3,18 +3,19 @@ package org.screenwork.minestomtest.space;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.command.builder.Command;
 import net.minestom.server.coordinate.Pos;
-import net.minestom.server.coordinate.Vec;
 import net.minestom.server.entity.Entity;
 import net.minestom.server.instance.Instance;
 import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.InstanceManager;
-import net.minestom.server.instance.generator.GenerationUnit;
+import net.minestom.server.instance.block.Block;
 import net.minestom.server.network.packet.server.play.ParticlePacket;
 import net.minestom.server.particle.Particle;
 import net.minestom.server.particle.ParticleCreator;
-import net.minestom.server.utils.binary.BinaryWriter;
+import net.minestom.server.utils.NamespaceID;
 import net.minestom.server.utils.time.TimeUnit;
+import net.minestom.server.world.DimensionType;
 
+import java.util.Objects;
 import java.util.Random;
 
 public class lightspeed extends Command {
@@ -22,7 +23,7 @@ public class lightspeed extends Command {
     public lightspeed() {
         super("lightspeed");
         setDefaultExecutor((sender, context) -> {
-            sender.sendMessage("Usage:  /lightspeed");
+            sender.sendMessage("Usage: /lightspeed");
         });
 
         addSyntax((sender, context) -> {
@@ -37,12 +38,16 @@ public class lightspeed extends Command {
     }
 
     private void createCustomDimension(Entity player) {
+        MinecraftServer.getDimensionTypeManager().addDimension(DimensionType.builder(NamespaceID.from("lightspeed")).build());
         InstanceManager instanceManager = MinecraftServer.getInstanceManager();
-        InstanceContainer customInstance = instanceManager.createInstanceContainer();
+        InstanceContainer customInstance = instanceManager.createInstanceContainer(Objects.requireNonNull(MinecraftServer
+                .getDimensionTypeManager().getDimension(NamespaceID.from("lightspeed"))));
+        customInstance.enableAutoChunkLoad(true);
+        customInstance.setGenerator(unit ->
+                unit.modifier().fill(Block.AIR));
         MinecraftServer.getInstanceManager().registerInstance(customInstance);
-        customInstance.setGenerator(GenerationUnit::modifier);
 
-        Pos teleportPosition = new Pos(0, 100, 0);
+        Pos teleportPosition = new Pos(0, 42, 0);
         player.setInstance(customInstance);
         player.teleport(teleportPosition);
 
