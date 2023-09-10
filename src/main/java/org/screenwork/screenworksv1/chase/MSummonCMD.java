ed75b24452ceadb5e67a17a77f63d091c2024596
cake5diamond <104602895+Calc-oholic@users.noreply.github.com>
2023-09-10 12:59:46 -0500

    this commit is a flex.

--- src/main/java/org/screenwork/screenworksv1/chase/MSummonCMD.java
+++ src/main/java/org/screenwork/screenworksv1/chase/MSummonCMD.java
@@ -1,75 +1,26 @@
 package org.screenwork.screenworksv1.chase;
 
-import com.extollit.gaming.ai.path.HydrazinePathFinder;
-import com.extollit.gaming.ai.path.model.IInstanceSpace;
-import com.extollit.gaming.ai.path.model.IPath;
-import com.extollit.gaming.ai.path.model.IPathingEntity;
-import com.extollit.linalg.immutable.Vec3d;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
-import net.minestom.server.MinecraftServer;
-import net.minestom.server.advancements.FrameType;
-import net.minestom.server.advancements.notifications.Notification;
-import net.minestom.server.advancements.notifications.NotificationCenter;
 import net.minestom.server.command.builder.Command;
-import net.minestom.server.coordinate.Pos;
-import net.minestom.server.entity.Entity;
-import net.minestom.server.entity.EntityType;
-import net.minestom.server.entity.Player;
-import net.minestom.server.entity.ai.goal.MeleeAttackGoal;
-import net.minestom.server.entity.ai.goal.RandomStrollGoal;
-import net.minestom.server.entity.ai.target.ClosestEntityTarget;
-import net.minestom.server.entity.ai.target.LastEntityDamagerTarget;
-import net.minestom.server.entity.metadata.display.ItemDisplayMeta;
-import net.minestom.server.entity.pathfinding.NavigableEntity;
-import net.minestom.server.entity.pathfinding.Navigator;
+import net.minestom.server.entity.EntityCreature;
 import net.minestom.server.instance.Instance;
-import net.minestom.server.item.ItemStack;
-import net.minestom.server.item.Material;
-import net.minestom.server.potion.Potion;
-import net.minestom.server.potion.PotionEffect;
-import org.screenwork.screenworksv1.Main;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
+import net.minestom.server.coordinate.Pos;
 
 public class MSummonCMD extends Command {
 
-    public ArrayList<Entity> memeList = new ArrayList<>();
-
     public MSummonCMD() {
-
         super("mSummon");
 
-        int min = 0;
-        int max = 5;
+        setDefaultExecutor((sender, context) -> {
+            Instance instance = sender.asPlayer().getInstance();
+            Pos position = sender.asPlayer().getPosition();
 
-        addSyntax((sender, context) -> {
-            int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
-            Entity chaser = new Entity(EntityType.ZOMBIE);
-            chaser.setNoGravity(true);
-            chaser.getEntityMeta().setInvisible(false);
-            chaser.setInstance(sender.asPlayer().getInstance());
-            chaser.teleport(new Pos(sender.asPlayer().getPosition().x(), sender.asPlayer().getPosition().y() + 0.1, sender.asPlayer().getPosition().z()));
-            ItemStack meme = ItemStack.builder(Material.PAPER).meta(metaBuilder -> {
-                metaBuilder.customModelData(randomNum);
-                metaBuilder.displayName(Component.text("Meme"));
-            }).build();
-            Entity chairItemDisplay = new Entity(EntityType.ITEM_DISPLAY);
-            ItemDisplayMeta displayMeta = (ItemDisplayMeta) chairItemDisplay.getEntityMeta();
-            displayMeta.setItemStack(meme);
-            displayMeta.setCustomName(Component.text("Display Item"));
-            chaser.addPassenger(chairItemDisplay);
-            memeList.add(chaser);
+            Meme meme = Meme.createNewMeme(instance, position);
 
-            // Pathfinding using Navigator
-            Navigator runner = new Navigator(chaser);
-            runner.setPathTo(new Pos(sender.asPlayer().getPosition().x() + 50, sender.asPlayer().getPosition().y(), sender.asPlayer().getPosition().z() + 50));
-            while ((MinecraftServer.getServer().isOpen()) && (!(runner == null))) {
-                runner.tick();
-            }
+            meme.setInstance(instance);
+
+            sender.sendMessage(Component.text("Summoned a meme!", NamedTextColor.GREEN));
         });
-
     }
 }
--- src/main/java/org/screenwork/screenworksv1/chase/Meme.java
+++ src/main/java/org/screenwork/screenworksv1/chase/Meme.java
@@ -1,31 +1,62 @@
 package org.screenwork.screenworksv1.chase;
 
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
-import net.minestom.server.command.builder.Command;
-import net.minestom.server.command.builder.arguments.ArgumentType;
-import net.minestom.server.command.builder.suggestion.SuggestionEntry;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.*;
-import net.minestom.server.entity.ai.goal.FollowTargetGoal;
-import net.minestom.server.entity.ai.goal.MeleeAttackGoal;
-import net.minestom.server.entity.ai.goal.RandomStrollGoal;
+import net.minestom.server.entity.ai.goal.*;
 import net.minestom.server.entity.ai.target.ClosestEntityTarget;
 import net.minestom.server.entity.ai.target.LastEntityDamagerTarget;
 import net.minestom.server.instance.Instance;
+import net.minestom.server.utils.entity.EntityFinder;
+import net.minestom.server.utils.math.IntRange;
 import net.minestom.server.utils.time.TimeUnit;
-import org.jetbrains.annotations.NotNull;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Meme extends EntityCreature {
 
     public Meme() {
         super(EntityType.ZOMBIE);
+        initializeAI();
+    }
+
+    private void initializeAI() {
         addAIGroup(
-                List.of(new FollowTargetGoal(this, Duration.of(1, TimeUnit.SERVER_TICK)), new MeleeAttackGoal(this, 1.6, 20, TimeUnit.SERVER_TICK), new RandomStrollGoal(this, 20)),
-                List.of(new LastEntityDamagerTarget(this, 32),  new ClosestEntityTarget(this, 32, entity -> entity instanceof Player))
+                List.of(
+                        new FollowTargetGoal(this, Duration.of(1, TimeUnit.SERVER_TICK)),
+                        new MeleeAttackGoal(this, 1.6, 20, TimeUnit.SERVER_TICK),
+                        new RandomStrollGoal(this, 20)
+                ),
+                List.of(
+                        new LastEntityDamagerTarget(this, 32),
+                        new ClosestEntityTarget(this, 32, entity -> entity instanceof Player)
+                )
         );
+    }
+
+    public static Meme createNewMeme(Instance instance, Pos position) {
+        Meme meme = new Meme();
+        meme.setInstance(instance);
+        meme.teleport(position);
+        return meme;
+    }
+
+    public static List<Pos> findMemesInRadius(Instance instance, Pos center, double radius) {
+        EntityFinder entityFinder = new EntityFinder()
+                .setTargetSelector(EntityFinder.TargetSelector.ALL_ENTITIES)
+                .setEntitySort(EntityFinder.EntitySort.NEAREST)
+                .setStartPosition(center)
+                .setDistance(new IntRange(0, (int) radius))
+                .setEntity(EntityType.ZOMBIE, EntityFinder.ToggleableType.INCLUDE);
+
+        List<Pos> memeLocations = new ArrayList<>();
+        entityFinder.find(instance, null).forEach(entity -> memeLocations.add(entity.getPosition()));
+        return memeLocations;
+    }
+
+
+    public void killMeme() {
+        this.remove();
     }
 }
