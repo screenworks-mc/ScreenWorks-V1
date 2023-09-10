bd644c9a1edea8dcd8f218f1404a52af38fe20fa
xDadxOffical <hypixelfan2@gmail.com>
2023-09-10 12:55:13 -0500

    Effect almost finished

--- src/main/java/org/screenwork/screenworksv1/testing/xdadx/EffectsCMD.java
+++ src/main/java/org/screenwork/screenworksv1/testing/xdadx/EffectsCMD.java
@@ -1,6 +1,10 @@
 package org.screenwork.screenworksv1.testing.xdadx;
 
 import net.minestom.server.command.builder.Command;
+import net.minestom.server.command.builder.arguments.ArgumentType;
+import net.minestom.server.command.builder.suggestion.SuggestionEntry;
+import net.minestom.server.potion.PotionEffect;
+import net.minestom.server.potion.PotionType;
 
 import java.util.List;
 
@@ -15,6 +19,25 @@ public class EffectsCMD extends Command {
 
         List<String> effectname = new java.util.ArrayList<>(List.of(new String[]{}));
 
+        for (PotionType value : PotionType.values()) {
+            effectname.add(value.name());
+
+        }
+
+        var effectArg = ArgumentType.Word("effect").from(effectname.toArray(new String[0]));
+
+        effectArg.setSuggestionCallback((sender, context, suggestion) -> {
+            for (PotionType potion : PotionType.values()) {
+                suggestion.addEntry(new SuggestionEntry(potion.name()));
+            }
+        });
+
+        addSyntax((sender, context) -> {
+
+            final String giveEffect = context.get(effectArg);
+
+
+        });
 
     }
 }
