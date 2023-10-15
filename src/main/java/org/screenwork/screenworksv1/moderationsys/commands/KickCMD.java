4b51f420d558d2d3b44b31bbe9ed9679c56db9b1
xDadxOffical <hypixelfan2@gmail.com>
2023-10-14 19:50:45 -0500

    Added more kick reasons o-o

--- src/main/java/org/screenwork/screenworksv1/moderationsys/commands/KickCMD.java
+++ src/main/java/org/screenwork/screenworksv1/moderationsys/commands/KickCMD.java
@@ -29,6 +29,11 @@ public class KickCMD extends Command {
             suggestion.addEntry(new SuggestionEntry("\"Hacking\""));
             suggestion.addEntry(new SuggestionEntry("\"Staff Disrespect\""));
             suggestion.addEntry(new SuggestionEntry("\"Being Annoying\""));
+            suggestion.addEntry(new SuggestionEntry("\"Spamming\""));
+            suggestion.addEntry(new SuggestionEntry("\"Slurs\""));
+            suggestion.addEntry(new SuggestionEntry("\"Advertising\""));
+            suggestion.addEntry(new SuggestionEntry("\"Caps\""));
+            suggestion.addEntry(new SuggestionEntry("\"Other\""));
         });
 
         addSyntax((sender, context) -> {
